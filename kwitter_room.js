38563d15476766c636417108add5f656cce84c3b
dhanush-danny <78251450+dhanush-danny@users.noreply.github.com>
2021-08-27 08:02:31 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,16 +14,16 @@ firebase.initializeApp(firebaseConfig);
 
 function getData() {
     firebase.database().ref("/").on('value', function (snapshot) {
-          document.getElementById("output").innerHTML = "";
-          snapshot.forEach(function (childSnapshot) {
-                childKey = childSnapshot.key;
-                Room_names = childKey;
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
 
-                //Start code
-                row = '<div class="room_name" id="' + Room_names + '" onclick="redirect(this.id)">' + Room_names + '</div><hr>';
-                document.getElementById("output").innerHTML += row;
-                //End code
-          });
+            //Start code
+            row = '<div class="room_name" id="' + Room_names + '" onclick="redirect(this.id)">' + Room_names + '</div><hr>';
+            document.getElementById("output").innerHTML += row;
+            //End code
+        });
     });
 }
 getData();
@@ -32,3 +32,25 @@ user_name = localStorage.getItem("user_name");
 
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
+function add_room() {
+    room_name = document.getElementById("room_name").value;
+    localStorage.setItem("room_name_key", room_name);
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "room folder created"
+    });
+    window.location = "kwitter_page.html";
+    document.getElementById("room_name").value = "";
+
+}
+
+function redirect(roomid) {
+    localStorage.setItem("room_name_key", roomid);
+    window.location = "kwitter_page.html";
+
+}
+
+function logout() {
+    localStorage.removeItem("room_name_key");
+    localStorage.removeItem("user_name_key");
+    window.location = "index.html";
+}
